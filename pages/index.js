1809a3c5c81efa7d94389b71497b5c7f9821ee84
dholmes2409 <daniel.holmes2409@gmail.com>
2022-02-07 21:58:02 +0000

    custom t3arbanner

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getPosts } from '../src/posts'
 import Link from 'next/link'
+import BlogHeader from '../src/components/organisms/BlogHeader'
 
 export async function getStaticProps(context) {
   const posts = await getPosts();
@@ -19,15 +20,18 @@ export async function getStaticProps(context) {
 };
 
 const PostList = (props) => (
-  <ul>
-    {props.posts.map(post => (
-      <li key={post.id}>
-        <Link href={`/${post.slug}`}>
-          <a>{post.title}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
+    <div>
+      <BlogHeader />
+      <ul>
+        {props.posts.map(post => (
+          <li key={post.id}>
+            <Link href={`/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
 );
 
 export default PostList;
